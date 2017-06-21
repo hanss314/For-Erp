815cb8d94a434f5f93fbc33d82a4c39bf884aa3a
hanss314 <hanss314@gmail.com>
2017-06-21 10:14:36 -0700

    Add files via upload

---
+++ server/server.py
@@ -0,0 +1,44 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import cgi
+
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+ 
+    # GET
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+    
+        message = ''
+        with open('index.html','r') as reader:
+            message = reader.read()
+        # Write content as utf-8 data
+        self.send_message(message)
+        return
+        
+    def do_POST(self):
+        print('posted')
+        content_len=int(self.headers.get('content-length',0))
+        post_body = self.rfile.read(content_len)
+        self.wfile.write(post_body)
+        print(post_body.decode())
+    
+    def send_message(self,message):
+        self.wfile.write(bytes(message, "utf8"))
+ 
+def run():
+    print('starting server...')
+
+    # Server settings
+    # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+    server_address = ('127.0.0.1', 8081)
+    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+    print('running server...')
+    httpd.serve_forever()
+ 
+ 
+run()
